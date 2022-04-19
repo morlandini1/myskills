059c1cac0ffaf8d500d31f7250e3e4633ada2099
MARCIO ORLANDINI <morlandini1@gmail.com>
2022-04-19 13:32:47 -0300

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+export default function Home(){
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Marcio</Text>
+
+      <TextInput style={styles.input}
+
+      />
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+       backgroundColor: '#121015', 
+       paddingVertical: 70, 
+       paddingHorizontal: 30,
+    },
+    title: {
+      color: '#FFF',
+      fontSize: 24,
+      fontWeight: "bold"
+    },
+    input: {
+      backgroundColor: '#1F1e25',
+      color: '#FFF',
+      fontSize: 18,
+      padding: 15,
+      margintop: 30,
+      borderRadius: 5
+    }
+});
