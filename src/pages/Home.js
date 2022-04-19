370d416b2f58d1d0622a6da29387bb3f77f90eca
MARCIO ORLANDINI <morlandini1@gmail.com>
2022-04-19 13:55:34 -0300

    Update Home.js
    Import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TextInput, 
+  Platform,
+  TouchableOpacity
+} from "react-native";
 
 export default function Home(){
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Marcio</Text>
 
-      <TextInput style={styles.input}
+      <TextInput 
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
 
       />
 
@@ -30,8 +40,8 @@ const styles = StyleSheet.create({
       backgroundColor: '#1F1e25',
       color: '#FFF',
       fontSize: 18,
-      padding: 15,
-      margintop: 30,
-      borderRadius: 5
+      padding: Platform.OS === 'ios' ? 15 : 10,
+      marginTop: 30,
+      borderRadius: 7
     }
 });
